ea244bcb2a455c29d7106604cc2f7a70a87e0ca1
PeiniCheng <90059889+PeiniCheng@users.noreply.github.com>
2022-11-05 20:22:35 -0400

    set up db

--- JCbet-Backend/src/main/java/peini/jcbet/JCbetBackendApplication.java
+++ JCbet-Backend/src/main/java/peini/jcbet/JCbetBackendApplication.java
@@ -2,12 +2,19 @@ package peini.jcbet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class JCbetBackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(JCbetBackendApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting() {
+    return "hello";
   }
 
 }
